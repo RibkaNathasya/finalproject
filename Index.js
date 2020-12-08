4aa9febe99ad2dcc28b84cd028f0fbe6e892c146
RibkaNathasya <70162184+RibkaNathasya@users.noreply.github.com>
2020-12-08 15:56:19 +0800

    Final Project (cv)
    curiculum vitae

---
+++ Index.js
@@ -0,0 +1,12 @@
+function curiculumvitae(){
+    document.querySelector(".biography").style.color = "#b088f9";
+    document.querySelector(".educations").style.color = "#b088f9";
+    document.querySelector(".freelance").style.color = "#b088f9";
+    document.querySelector(".contactme").style.color = "#b088f9";
+    
+    document.querySelector(".cv").style.backgroundColor="#bedcfa";
+}
+
+function displaytime(){
+    document.getElementById("time").innerHTML = Date();
+}
